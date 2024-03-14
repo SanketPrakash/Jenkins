d208fd91befd17f2ad3aa194155ea9bdad8acaab
Sanket Prakash <65389672+SanketPrakash@users.noreply.github.com>
2024-03-15 00:31:00 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.sql.Date;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("java Program");
+        System.out.println("i love jenkins");
+        System.out.println(new Date(1));
+    }
+
+}
