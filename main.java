7fcdca03ea739a90c8e4a7422667980f8bb48301
Sanket Prakash <65389672+SanketPrakash@users.noreply.github.com>
2024-03-15 01:39:54 +0530

    second commit

--- main.java
+++ main.java
@@ -6,6 +6,8 @@ public class main {
         System.out.println("java Program");
         System.out.println("i love jenkins");
         System.out.println(new Date(1));
+        System.out.println(new Date(27));
+        System.out.println("Welcome sanket yu are nou in jenkins");
     }
 
 }
