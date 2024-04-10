8eb9030a17eb1f0f0d9ae4b66e39a81efcc608ea
Sanket Prakash <65389672+SanketPrakash@users.noreply.github.com>
2024-04-10 23:22:52 +0530

    5th Commit

--- student.java
+++ student.java
@@ -1,10 +1,10 @@
 public class student {
     public student() {
-        System.out.println("Hello Im cnstructor");
+        System.out.println("Hello Im constructor");
     }
 
     public void hello() {
-        System.out.println("Hye kaisse ho");
+        System.out.println("Hye kaisse ho or tum batao?");
     }
 
 }
